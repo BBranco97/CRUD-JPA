5dfa62be76fd0b9cb67cc0a8fcff35ee98ae7a0a
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-03-09 18:56:46 -0300

    JPA notations

---
+++ src/main/java/pw3_ead/Aluno.java
@@ -0,0 +1,75 @@
+package pw3_ead;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Aluno {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String ra;
+    private String email;
+    private BigDecimal nota1;
+    private BigDecimal nota2;
+    private BigDecimal nota3;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(BigDecimal nota1) {
+        this.nota1 = nota1;
+    }
+
+    public BigDecimal getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(BigDecimal nota2) {
+        this.nota2 = nota2;
+    }
+
+    public BigDecimal getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(BigDecimal nota3) {
+        this.nota3 = nota3;
+    }
+}
