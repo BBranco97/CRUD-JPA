e032acf1142922462403ad58fedd65426e84ca0a
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-03-09 22:53:51 -0300

    add db

---
+++ src/main/java/pw3_ead/dao/AlunoDAO.java
@@ -0,0 +1,17 @@
+package pw3_ead.dao;
+
+import jakarta.persistence.EntityManager;
+import pw3_ead.modelo.Aluno;
+
+public class AlunoDAO {
+        // EntityManager, que será usado por todos os métodos:
+        private EntityManager em;
+        // Construtor que já recebe o EntityManager criado:
+        public AlunoDAO(EntityManager em) {
+            this.em = em;
+        }
+        // Método para gravar um produto no BD:
+        public void cadastrar(Aluno aluno) {
+            this.em.persist(aluno);
+        }
+}
--- src/main/java/pw3_ead/modelo/Aluno.java
+++ src/main/java/pw3_ead/modelo/Aluno.java
@@ -1,7 +1,6 @@
-package pw3_ead;
+package pw3_ead.modelo;
 
 import jakarta.persistence.*;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigDecimal;
@@ -20,13 +19,20 @@ public class Aluno {
     private BigDecimal nota2;
     private BigDecimal nota3;
 
+
+    public Aluno(String nome, String ra, String email, BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
+        this.nome = nome;
+        this.ra = ra;
+        this.email = email;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
@@ -76,35 +82,5 @@ public class Aluno {
         this.nota3 = nota3;
     }
 
-    public static void main(String[] args){
 
-        SpringApplication.run(Aluno.class, args);
-
-        // Criando um objeto da Classe Produto:
-        Aluno a1 = new Aluno();
-        a1.setNome("Amanda");
-        a1.setEmail("amanda@gmail.com");
-        a1.setRa("1");
-        a1.setNota1(new BigDecimal(6.0));
-        a1.setNota2(new BigDecimal(7.0));
-        a1.setNota3(new BigDecimal(8.0));
-
-        // Criando uma factory de EntityManager:
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("cadastro_alunos");
-
-        // Usando a factory acima pra criar o objeto EntityManager:
-        EntityManager em = factory.createEntityManager();
-
-        // Iniciando uma transação:
-        em.getTransaction().begin();
-
-        // Gravando o objeto no banco de dados:
-        em.persist(a1);
-
-        // "Comitando" a transação:
-        em.getTransaction().commit();
-
-        // Fechando este EntityManager, já que não precisaremos mais dele:
-        em.close();
-    }
 }
