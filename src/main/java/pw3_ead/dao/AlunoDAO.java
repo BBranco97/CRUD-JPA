af91b4eeb0f90bda76fb60de475338e8a13246a1
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-03-11 22:06:41 -0300

    remove comments

--- src/main/java/pw3_ead/dao/AlunoDAO.java
+++ src/main/java/pw3_ead/dao/AlunoDAO.java
@@ -6,26 +6,24 @@ import pw3_ead.modelo.Aluno;
 import java.util.List;
 
 public class AlunoDAO {
-        // EntityManager, que será usado por todos os métodos:
         private EntityManager em;
-        // Construtor que já recebe o EntityManager criado:
         public AlunoDAO(EntityManager em) {
             this.em = em;
         }
-        // Método para gravar um produto no BD:
         public void cadastrar(Aluno aluno) {
             this.em.persist(aluno);
         }
-        // Método para Excluir um produto no BD:
         public void excluir(Aluno aluno){
                 this.em.remove(aluno);
         }
+
         public List<Aluno> buscarPorNome(String nome) {
                 String jpql = "SELECT a FROM Aluno a WHERE a.nome = :n";
                 return em.createQuery(jpql, Aluno.class)
                         .setParameter("n", nome)
                         .getResultList();
         }
+
         public Aluno buscarUmPorNome(String nome){
                 String jpql = "SELECT a FROM Aluno a WHERE a.nome =:n";
                 return em.createQuery(jpql, Aluno.class).setParameter("n", nome).getSingleResult();
